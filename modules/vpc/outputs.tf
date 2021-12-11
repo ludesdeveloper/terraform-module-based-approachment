bb3638182f69cbdd0db0cacf2aa72473996bfda1
FunGuardian <seski.ramadhan@gmail.com>
2021-12-11 21:12:55 +0700

    adding inet gateway output

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -9,3 +9,7 @@ output "private_subnet_id" {
 output "public_subnet_id" {
   value = [for i in aws_subnet.public_subnet : i.id]
 }
+
+output "internet_gateway" {
+  value = aws_internet_gateway.internet_gateway
+}
